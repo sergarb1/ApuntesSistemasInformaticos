11d62d50309aae6d00b1179db5eb7a31e0ddba6e
sergarb1 <sergi.profesor@gmail.com>
2023-01-18 16:46:07 +0100

    Actualizado

--- English OLD/UD12. (Extra) Bash Scripting/UD12 Solutions/UD12 Activities I/forum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#We iniciate forum to an empty string
-forum=""
-#While forum is different from GOOD, we repeat the question
-while [ "$forum" != "GOOD" ]
-do
-	read -p "What do you think about or forum?: " forum
-done
-echo "Thanks for your comments ;)"
--- English OLD/UD12. (Extra) Bash Scripting/UD12 Solutions/UD12 Activities II/countries.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#We filter by country names and then we order it descendently (-r) with unique values (-u)
-cut -d":" -f 2 countries.txt | sort -r -u
